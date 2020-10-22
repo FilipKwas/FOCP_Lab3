db52d29fd5bce4ba590894a6e41176271d98a061
FilipKwas <filikwa678@student.polsl.pl>
2020-10-22 12:24:11 +0200

    Initial skeletonfor struct

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,4 +1,25 @@
+#include <array>
+#include <iostream>
+
+using namespace std;
+// Define a struct
+struct Point2D {
+	double x;
+	double y;
+};
+
 int main() {
-	
+	// Deckare a varuabke with our struct
+	Point2D mypoint; 
+		
+	mypoint.x = 3.3;
+	mypoint.y = 5.5;
+		// Print its content
+		cout << "Value of X: " << mypoint.y << endl;
+		cout << "Value of Y: " << mypoint.y << endl;
+
+
+
+
 	return 0;
 }
