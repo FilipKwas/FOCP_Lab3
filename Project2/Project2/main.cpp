89228e77672f60fdd92683631ccd9a96c38645a3
FilipKwas <filikwa678@student.polsl.pl>
2020-10-22 15:08:28 +0200

    Next commit about matrixes

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -7,14 +7,65 @@ using namespace std;
 struct Matrix2D {
 	vector<vector<int>> contents;
 
+	void add(Matrix2D other) {
+		if (other.rows() == rows() && other.columns() == columns()) {
+			
+			for (int i = 0; i < rows(); i++) {
+				for (int j = 0; j < columns(); j++) {
+					contents[i][j] += other.contents[i][j];
+				}
+			}
+		
+		} else {
+			cout << "Both matrices must have same number of rows!" << endl;
+		}
+	}
+
+	int rows() {
+		return contents.size();
+
+	}
+
+	int columns(){
+
+		return contents[0].size();
+
+	}
+
+
+	void input_matrix() {
+	
+		int columns, rows;
+
+		cout << "Number of rows: " << endl;
+		cin >> rows;
+		
+		cout << "Number of columns: " << endl;
+		cin >> columns;
+
+		for (int i = 0; i < rows; i++) {
+			vector<int> row;
+			int tmp;
+			cout << "Adding a row with " << columns << " columns";
+			for (int j = 0; j < columns; j++) {
+				cout << "Introduce value: " << endl;
+				cin >> tmp;
+				row.push_back(tmp);
+
+
+			}
+			contents.push_back(row);
+		}
+	}
+
 	void print() {
 		cout << "Contents of the matrix: " << endl;
 		
 		int rows = contents.size();
 		int cols = contents[0].size();
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
 				cout << contents[i][j] << " ";
 			}
 			cout << endl;
@@ -23,19 +74,25 @@ struct Matrix2D {
 };
 
 int main() {
-	// A 3x3, B 3x3
+	Matrix2D a, b;
 	
-	// Declare the matrices
-	Matrix2D a;
-	Matrix2D b;
+	cout << "Intoduce data for matrix a: " << endl;
+	
+	a.input_matrix();
+	
+	cout << "Intoduce data for matrix b: " << endl;
+	
+	b.input_matrix();
 
-	// Initialize the contents
-	a.contents = { {1,2,3}, {4,5,6}, {7,8,9} };
-	b.contents = { {9,8, 7}, {6,5,4}, {3,2,1} };
-
+	cout << "Print matrix a: " << endl;
 	a.print();
+	
+	cout << "Print matrix b: " << endl;
 	b.print();
-
+	
+	cout << "Adding b to a: " << endl;
+	a.add(b);
+	a.print();
 
 	return 0;
 }
