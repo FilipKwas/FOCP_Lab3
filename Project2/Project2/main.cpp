1ba0ee6ade7240cea88694bc6e800d752ce7f258
FilipKwas <filikwa678@student.polsl.pl>
2020-10-22 12:38:55 +0200

    Funtion to initialize point

--- Project2/Project2/main.cpp
+++ Project2/Project2/main.cpp
@@ -1,5 +1,5 @@
-#include <array>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 // Define a struct
@@ -8,18 +8,36 @@ struct Point2D {
 	double y;
 };
 
+// INPUT ---> {????} ---> OUTPUT
+//		{initialize struct}
+
+Point2D create_point() {
+	Point2D tmp;
+	tmp.x = rand() % 10;
+	tmp.y = rand() % 10;
+
+	return tmp;
+}
+
+void initializa_in_place (Point2D& point) {
+	point.x = rand() % 10;
+	point.y = rand() % 10;
+	cout << "Value of X: " << point.x << endl;
+	cout << "Value of Y: " << point.y << endl;
+}
+
 int main() {
-	// Deckare a varuabke with our struct
-	Point2D mypoint; 
-		
-	mypoint.x = 3.3;
-	mypoint.y = 5.5;
-		// Print its content
-		cout << "Value of X: " << mypoint.y << endl;
-		cout << "Value of Y: " << mypoint.y << endl;
+	
+	Point2D mypoint = create_point();
 
+	cout << "Value of X: " << mypoint.x << endl;
+	cout << "Value of Y: " << mypoint.y << endl;
 
-
-
+	Point2D empty_point = create_point();
+	initializa_in_place(empty_point);
+	
+	cout << "Value of X: " << empty_point.y << endl;
+	cout << "Value of Y: " << empty_point.y << endl;
+	
 	return 0;
 }
